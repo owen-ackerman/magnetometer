4bc4d179157f8788a87932543bd224bedc13b9ea
Owen Ackerman <ackerman01owen@gmail.com>
2020-12-12 00:08:03 -0500

    Mag

--- Magnetometer_Test.py
+++ Magnetometer_Test.py
@@ -9,10 +9,8 @@ SENSOR = adafruit_mlx90393.MLX90393(I2C_BUS, gain=adafruit_mlx90393.GAIN_1X)
  
 while True:
     MX, MY, MZ = SENSOR.magnetic
-    print("[{}]".format(time.monotonic()))
-    print("X: {} uT".format(MX))
-    print("Y: {} uT".format(MY))
-    print("Z: {} uT".format(MZ))
+    #print("[{}]".format(time.monotonic()))
+    print("X: {} uT".format(MX)), ("Y: {} uT".format(MY)),  ("Z: {} uT".format(MZ))
     # Display the status field if an error occured, etc.
     if SENSOR.last_status > adafruit_mlx90393.STATUS_OK:
         SENSOR.display_status()
